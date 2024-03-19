df61becae13dcf30c22c2a409c2b0fe58f06cb36
Chris <chrishead22@gmail.com>
2024-03-18 20:54:27 -0600

    updating unit tests / hopefully fixing .gitignore

---
+++ _BoredTests/DataCollectorTests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Net;
+using Xunit;
+
+namespace BoredTests;
+
+public class DataCollectorTests
+{
+    [Fact]
+    public void GetApiResponse_OK()
+    {
+        string url = "https://www.boredapi.com/api/activity";
+        HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+        HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+        
+        Assert.Equal("OK", response.StatusCode.ToString());
+    }
+}
